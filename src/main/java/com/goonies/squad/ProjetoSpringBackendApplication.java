44708e521fac4070cab605122f3217f2f75ca8ba
Anderson Fagundes Gimenes <anderson.fagundes.gimenes@gmail.com>
2021-02-22 16:27:04 -0300

    Projeto Criado

---
+++ src/main/java/com/goonies/squad/ProjetoSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.goonies.squad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goonies/squad/ProjetoSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goonies.squad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
