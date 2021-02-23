8e28dc857a4f5db91978af9def34907adaf16c36
Anderson Fagundes Gimenes <anderson.fagundes.gimenes@gmail.com>
2021-02-22 23:55:44 -0300

    Criando operacao de instanciacao

--- src/main/java/com/goonies/squad/ProjetoSpringBackendApplication.java
+++ src/main/java/com/goonies/squad/ProjetoSpringBackendApplication.java
@@ -1,13 +1,55 @@
 package com.goonies.squad;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.goonies.squad.domain.Categoria;
+import com.goonies.squad.repositories.CategoriaRepository;
+
+
 @SpringBootApplication
-public class ProjetoSpringBackendApplication {
+public class ProjetoSpringBackendApplication implements CommandLineRunner{
+	
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBackendApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		
+		
+				
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+		
+		
+	
+	}
+	
+	}
+
